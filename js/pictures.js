0f5771bcfbf107c404309766f8423dcb454532a1
Artem Kartavtsev <arkartavtsev@live.ru>
2018-06-28 02:39:26 +0700

    Реализация механизма валидации формы загрузки фото

--- js/pictures.js
+++ js/pictures.js
@@ -13,6 +13,11 @@ var ResizeValues = {
   STEP: 25
 };
 
+var HastagsValidity = {
+  MAX_QUANTITY: 5,
+  MAX_LENGTH: 20
+};
+
 var PhotosProperties = {
   QUANTITY: 25,
   LIKES: {
@@ -212,7 +217,7 @@ var closePopup = function () {
 
 var onPhotoClick = function (evt) {
   if (evt.target.classList.contains('picture__img')) {
-    openPopup(photos[evt.target.parentNode.dataset.id]);
+    openPopup(photos[evt.target.parentElement.dataset.id]);
   }
 };
 
@@ -335,7 +340,77 @@ var onFilterChange = function (evt) {
 };
 
 
+var isBadTagsSeparation = function (arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf('#', 1) !== -1 || arr[i] === '') {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+var isBadTagFormat = function (arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].charAt(0) !== '#' || arr[i].length === 1) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+var isTooLongTag = function (arr, maxLength) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].length > maxLength) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+var isTagsRepeat = function (arr) {
+  var existingElements = {};
+
+  for (var i = 0; i < arr.length; i++) {
+    if (existingElements[arr[i]]) {
+      return true;
+    }
+
+    existingElements[arr[i]] = true;
+  }
+
+  return false;
+};
+
+var onTagsFieldValidate = function () {
+  var tags = tagsField.value.trim().toLowerCase().split(' ');
+
+  if (isBadTagsSeparation(tags)) {
+    tagsField.setCustomValidity('Хэш-теги должны разделяться одним пробелом');
+  } else if (isBadTagFormat(tags)) {
+    tagsField.setCustomValidity('Хэш-тег должен начинаться с символа # и не может состоять только из него');
+  } else if (isTooLongTag(tags, HastagsValidity.MAX_LENGTH)) {
+    tagsField.setCustomValidity('Максимальная длина одного хэш-тега ' + HastagsValidity.MAX_LENGTH + ' символов');
+  } else if (isTagsRepeat(tags)) {
+    tagsField.setCustomValidity('Хэш-теги не должны повторяться');
+  } else if (tags.length > HastagsValidity.MAX_QUANTITY) {
+    tagsField.setCustomValidity('Нельзя указывать больше ' + HastagsValidity.MAX_QUANTITY + ' хэш-тегов');
+  } else {
+    tagsField.setCustomValidity('');
+    tagsField.classList.remove('invalid-field');
+  }
+
+  if (tagsField.value && !tagsField.validity.valid) {
+    tagsField.classList.add('invalid-field');
+  }
+};
+
+
 decreaseBtn.addEventListener('click', onPhotoScale);
 increaseBtn.addEventListener('click', onPhotoScale);
 
 filtersContainer.addEventListener('click', onFilterChange);
+
+uploader.addEventListener('input', onTagsFieldValidate);
