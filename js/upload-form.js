b66698fc5ebb21531c9e3a22c74c8db9edec0c11
Artem Kartavtsev <arkartavtsev@live.ru>
2018-07-13 02:45:57 +0700

    Добавление возможности работы с загружаемым изображением

---
+++ js/upload-form.js
@@ -0,0 +1,61 @@
+'use strict';
+
+
+(function () {
+  var uploadForm = document.querySelector('.img-upload__form');
+  var submitBtn = uploadForm.querySelector('.img-upload__submit');
+
+
+  var sendForm = function () {
+    window.uploadPopup.close();
+    submitBtn.disabled = false;
+  };
+
+  var showError = function (errorMessage) {
+    var errorContainer = document.querySelector('.img-upload__message--error');
+    var errorText = errorContainer.querySelector('.error__message');
+    var tryAgainBtn = errorContainer.querySelector('.try-again-link');
+    var loadNewBtn = errorContainer.querySelector('.new-file-link');
+
+    var onTryAgainBtnClick = function (evt) {
+      evt.preventDefault();
+
+      submitBtn.disabled = false;
+
+      tryAgainBtn.removeEventListener('click', onTryAgainBtnClick);
+      loadNewBtn.removeEventListener('click', onLoadNewBtnClick);
+
+      errorContainer.classList.add('hidden');
+    };
+
+    var onLoadNewBtnClick = function (evt) {
+      evt.preventDefault();
+
+      window.uploadPopup.close();
+      submitBtn.disabled = false;
+
+      tryAgainBtn.removeEventListener('click', onTryAgainBtnClick);
+      loadNewBtn.removeEventListener('click', onLoadNewBtnClick);
+
+      errorContainer.classList.add('hidden');
+    };
+
+    errorText.textContent = 'Ошибка загрузки файла. ' + errorMessage;
+
+    tryAgainBtn.addEventListener('click', onTryAgainBtnClick);
+    loadNewBtn.addEventListener('click', onLoadNewBtnClick);
+
+    errorContainer.classList.remove('hidden');
+  };
+
+
+  var onFormSubmit = function (evt) {
+    evt.preventDefault();
+    submitBtn.disabled = true;
+
+    window.backend.save(new FormData(uploadForm), sendForm, showError);
+  };
+
+
+  uploadForm.addEventListener('submit', onFormSubmit);
+})();
